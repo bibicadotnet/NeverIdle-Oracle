78885979b89d98c965b57110e86daaf3d22fc104
bibica <119997727+bibicadotnet@users.noreply.github.com>
2024-01-11 03:25:09 +0700

    Update VM.Standard.A1.Flex.sh

--- VM.Standard.A1.Flex.sh
+++ VM.Standard.A1.Flex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bash <(curl -s -L https://gist.githubusercontent.com/Ansen/e45320205faf5786d3282ac880f20bab/raw/onekey-NeverIdle.sh)
 pkill NeverIdle
-/tmp/NeverIdle -c 12h -cp 0.15 -m 4 -n 12h
+/tmp/NeverIdle -c 12h -cp 0.2 -m 5 -n 12h
